cc0b158d1798270b2e6338f9957facdfebc7a665
bschuedzig <benjamin.schuedzig@mantro.net>
2019-05-20 14:55:20 +0200

    Updates scripts to work with standard 'cut'

--- git_find_big_files_in_history.sh
+++ git_find_big_files_in_history.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Find big files in the repo
 
-git rev-list --objects --all \
-| git cat-file --batch-check='%(objecttype) %(objectname) %(objectsize) %(rest)' \
-| awk '/^blob/ {print substr($0,6)}' \
-| grep -vF "$(git ls-tree -r HEAD | awk '{print $3}')" \
-| sort --numeric-sort --key=2 \
-| cut --complement --characters=13-40 \
-| numfmt --field=2 --to=iec-i --suffix=B --padding=7 --round=nearest
+git rev-list --objects --all |
+    git cat-file --batch-check='%(objecttype) %(objectname) %(objectsize) %(rest)' |
+    awk '/^blob/ {print substr($0,6)}' |
+    grep -vF "$(git ls-tree -r HEAD | awk '{print $3}')" |
+    sort --numeric-sort --key=2 |
+    cut -d " " -f2- |
+    numfmt --field=1 --to=iec-i --suffix=B --padding=7 --round=nearest |
+    tail -50
